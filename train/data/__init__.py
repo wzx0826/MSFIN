b3fb67f43e7a7991a86755fc95194f27aea2635e
Zhengxue Wang <54923578+wzx0826@users.noreply.github.com>
2022-04-22 10:18:45 +0800

    Update __init__.py

--- train/data/__init__.py
+++ train/data/__init__.py
@@ -19,7 +19,7 @@ class Data:
 
         if args.data_test in ['Set5', 'Set14', 'B100', 'Urban100']:
             module_test = import_module('data.benchmark')
-            testset = getattr(module_test, 'Benchmark')(args, name=args.data_test, train=False)
+            testset = getattr(module_test, 'Benchmark')(args, train=False)
         else:
             module_test = import_module('data.' +  args.data_test.lower())
             testset = getattr(module_test, args.data_test)(args, train=False)
